a6201be6af731c357d3961f87165af7a704223d2
nicknjagi <njaginicholas@gmail.com>
2023-07-19 13:33:29 +0300

    add routes and wrap routes with skeleton theme parent

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
+import React from 'react'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Layout from './components/Layout';
+import CountryDetailLayout from './components/CountryDetailLayout';
+import Home from './pages/home'
+import CountryDetail from './pages/countryDetail'
+import { SkeletonTheme } from 'react-loading-skeleton';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <SkeletonTheme
+      baseColor="rgba(235,235,235,0.7)"
+      highlightColor="rgba(235,235,235,0.5)">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/:name" element={<CountryDetailLayout />}>
+              <Route index element={<CountryDetail />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </SkeletonTheme>
   )
 }
 
